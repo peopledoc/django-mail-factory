716e142634b95a9b80cc08833383ba1af341048a
Florent Messa <florent.messa@gmail.com>
2013-01-25 23:50:48 +0100

    Don't need to call autodiscover manually

--- demo/demo/urls.py
+++ demo/demo/urls.py
@@ -1,9 +1,6 @@
 from django.conf.urls import patterns, include
 from django.contrib import admin
 
-from mail_factory import models as factory
-
-factory.autodiscover()
 admin.autodiscover()
 
 urlpatterns = patterns(
