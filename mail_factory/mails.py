b64111659153018948ae45c31bf9423e9999e39d
Florent Messa <florent.messa@gmail.com>
2013-02-22 16:43:51 +0100

    Add *lang* optional parameter to _render_part and create_email_msg to override the default get_language in BaseMail.

--- mail_factory/mails.py
+++ mail_factory/mails.py
@@ -77,7 +77,7 @@ class BaseMail(object):
         # return the list of templates path candidates
         return templates
 
-    def _render_part(self, part):
+    def _render_part(self, part, lang=None):
         """Render a mail part against the mail context.
 
         Part can be:
@@ -90,23 +90,24 @@ class BaseMail(object):
         tpl = select_template(self.get_template_part(part))
         cur_lang = translation.get_language()
         try:
-            translation.activate(self.lang)
+            translation.activate(lang or self.lang)
             rendered = tpl.render(self.context)
         finally:
             translation.activate(cur_lang)
         return rendered.strip()
 
-    def create_email_msg(self, emails, attachments=None, from_email=None):
+    def create_email_msg(self, emails, attachments=None, from_email=None,
+                         lang=None):
         """Create an email message instance."""
 
         from_email = from_email or settings.DEFAULT_FROM_EMAIL
-        subject = self._render_part('subject.txt').strip()
+        subject = self._render_part('subject.txt', lang=lang).strip()
         try:
-            body = self._render_part('body.txt')
+            body = self._render_part('body.txt', lang=lang)
         except TemplateDoesNotExist:
             body = ''
         try:
-            html_content = self._render_part('body.html')
+            html_content = self._render_part('body.html', lang=lang)
         except TemplateDoesNotExist:
             html_content = None
 
