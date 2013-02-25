93e22766340eea806532bb60d64ff3305554292e
Mathieu Agopian <mathieu.agopian@gmail.com>
2013-02-25 15:45:03 +0100

    proper flake8 checking (--ignore are for the wimps ;)

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
 flake8:
-	flake8 mail_factory --ignore=E501,E127,E128,E124
+	flake8 mail_factory
 
 test:
 	coverage run --branch --source=mail_factory demo/manage.py test mail_factory
--- mail_factory/urls.py
+++ mail_factory/urls.py
@@ -6,6 +6,10 @@ from django.conf import settings
 
 from mail_factory.views import mail_list, form, preview_message
 
+
+LANGUAGE_CODES = '|'.join([code for code, name in settings.LANGUAGES])
+
+
 urlpatterns = patterns(
     '',
     url(r'^$',
@@ -14,7 +18,7 @@ urlpatterns = patterns(
     url(r'^(?P<mail_name>[-\w]+)/$',
         form,
         name='mail_factory_form'),
-    url(r'^(?P<mail_name>[-\w]+)/preview/(?P<lang>(%s))/$' % '|'.join([code for code, name in settings.LANGUAGES]),
+    url(r'^(?P<mail_name>[-\w]+)/preview/(?P<lang>(%s))/$' % LANGUAGE_CODES,
         preview_message,
         name='mail_factory_preview_message'),
 )
