094bb56a01d4cdc155ac1b84c44653d479cd0770
Florent Messa <florent.messa@gmail.com>
2013-02-25 14:38:46 +0100

    Add `get_mail_preview` to retrieve a PreviewMail class. Make `get_mail_class` and `get_mail_object` public.

--- mail_factory/factory.py
+++ mail_factory/factory.py
@@ -54,7 +54,7 @@ class MailFactory(object):
         if key in self.preview_map:
             del self.preview_map[key]
 
-    def _get_mail_class(self, template_name):
+    def get_mail_class(self, template_name):
         """Returns the MailClass from the registration map and its
         template_name."""
         if not template_name in self.mail_map:
@@ -63,14 +63,14 @@ class MailFactory(object):
 
         return self.mail_map[template_name]
 
-    def _get_mail_object(self, template_name, context=None):
+    def get_mail_object(self, template_name, context=None):
         """Returns the MailClass instance from the registration map and its
         template_name."""
-        MailClass = self._get_mail_class(template_name)
+        MailClass = self.get_mail_class(template_name)
 
         return MailClass(context)
 
-    def _get_mail_form(self, template_name):
+    def get_mail_form(self, template_name):
         """Returns the MailForm instance from the registration map and its
         template_name."""
         if template_name in self.mail_map:
@@ -81,34 +81,45 @@ class MailFactory(object):
                     "%s is registered but doesn't have a form."
                     "Please restart your server" % template_name)
 
+    def get_mail_preview(self, template_name):
+        """Returns the PreviewMail instance from the registration map and its
+        template_name."""
+        if template_name in self.mail_map:
+            try:
+                return self.preview_map[template_name]
+            except KeyError:
+                raise exceptions.MailFactoryError(
+                    "%s is registered but doesn't have a form."
+                    "Please restart your server" % template_name)
+
     def mail(self, template_name, emails, context,
              attachments=None, from_email=None):
         """Send a mail from the template_name."""
-        self._get_mail_object(template_name, context) \
+        self.get_mail_object(template_name, context) \
             .send(emails, attachments, from_email)
 
     def mail_admins(self, template_name, context,
                     attachments=None, from_email=None):
         """Send the mail to admins."""
-        self._get_mail_object(template_name, context) \
+        self.get_mail_object(template_name, context) \
             .mail_admins(attachments, from_email)
 
     def get_html_for(self, template_name, context):
         """Preview the body.html mail."""
-        return self._get_mail_object(template_name, context) \
+        return self.get_mail_object(template_name, context) \
                    ._render_part('body.html')
 
     def get_text_for(self, template_name, context):
         """Return the rendered mail text body."""
-        return self._get_mail_object(template_name, context) \
+        return self.get_mail_object(template_name, context) \
                    ._render_part('body.txt')
 
     def get_subject_for(self, template_name, context):
         """Return the rendered mail subject."""
-        return self._get_mail_object(template_name, context) \
+        return self.get_mail_object(template_name, context) \
                    ._render_part('subject.txt')
 
     def get_raw_content(self, template_name, emails, context, from_email=None):
         """Return raw mail source before sending."""
-        return self._get_mail_object(template_name, context) \
+        return self.get_mail_object(template_name, context) \
                    .create_email_msg(emails, from_email=from_email)
