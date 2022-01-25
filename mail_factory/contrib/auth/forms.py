db9beb07707eef165846e59af3c7efe5d2a14c08
Robin Cherbonnier <robin.cherbonnier@ukg.com>
2022-01-25 10:31:56 +0100

    compatibility with Django 3.0

--- mail_factory/contrib/auth/forms.py
+++ mail_factory/contrib/auth/forms.py
@@ -3,7 +3,7 @@
 from django.contrib.auth.forms import PasswordResetForm as DjangoPasswordResetForm
 from django.contrib.auth.tokens import default_token_generator
 from django.contrib.sites.shortcuts import get_current_site
-from django.utils.encoding import force_bytes, force_text
+from django.utils.encoding import force_bytes, force_str
 from django.utils.http import urlsafe_base64_encode
 
 from mail_factory import factory
@@ -40,7 +40,7 @@ class PasswordResetForm(DjangoPasswordResetForm):
                 "email": email,
                 "domain": domain,
                 "site_name": site_name,
-                "uid": force_text(urlsafe_base64_encode(force_bytes(user.pk))),
+                "uid": force_str(urlsafe_base64_encode(force_bytes(user.pk))),
                 "user": user,
                 "token": token_generator.make_token(user),
                 "protocol": "https" if use_https else "http",
