2443320ad551083909e305614657cf9e0598d9a4
Robin Cherbonnier <robin.cherbonnier@ukg.com>
2022-01-25 10:23:00 +0100

    black and isort on demo/

--- demo/demo_app/mails.py
+++ demo/demo_app/mails.py
@@ -1,24 +1,22 @@
-from mail_factory import factory, BaseMail, MailForm
+from mail_factory import BaseMail, MailForm, factory
 
 
 class CustomFormMail(BaseMail):
-    template_name = 'custom_form'
-    params = ['title', 'content']
+    template_name = "custom_form"
+    params = ["title", "content"]
 
 
 class CustomFormMailForm(MailForm):
-
     class Meta:
-        initial = {
-            'title': 'My initial subject',
-            'content': 'My initial content'
-        }
+        initial = {"title": "My initial subject", "content": "My initial content"}
+
 
 factory.register(CustomFormMail, mail_form=CustomFormMailForm)
 
 
 class NoCustomMail(BaseMail):
-    template_name = 'no_custom'
-    params = ['title', 'content']
+    template_name = "no_custom"
+    params = ["title", "content"]
+
 
 factory.register(NoCustomMail)  # default form
--- demo/urls.py
+++ demo/urls.py
@@ -1,8 +1,7 @@
-from django.conf.urls import url, include
+from django.conf.urls import include, url
 from django.contrib import admin
 
-
 urlpatterns = [
-    url(r'^mail_factory/', include('mail_factory.urls')),
-    url(r'^admin/', admin.site.urls),
+    url(r"^mail_factory/", include("mail_factory.urls")),
+    url(r"^admin/", admin.site.urls),
 ]
